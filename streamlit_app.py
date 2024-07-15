678a5cd454e8c4145efc33a230c07fd0dec3acfa
Gabriel.T <gabrielfernandesthiago@hotmail.com>
2024-07-15 10:55:39 -0300

    refactoring

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,101 +3,45 @@ import pandas as pd
 import streamlit as st
 import datetime
 import numpy as np
-
 import folium
 from streamlit_folium import folium_static
-
 from scipy.spatial import cKDTree
-
+import numpy as np
+from folium import Choropleth
 import json
 
-# Show the page title and description.
-st.set_page_config(page_title="Dados Meteorológicos Simepar Paraná", page_icon="☁️")
-st.title("Dados Meteorológicos Simepar Paraná")
-st.write(
-    """
-    Este aplicativo visualiza dados do [SIMEPAR](http://www.simepar.br/prognozweb/simepar).
-    Ele mostra métricas sobre o clima coletadas em estações meteorológicas no Paraná-RS.
-    """
-)
+# --------------- Page config ---------------
+page_title = "Dados Meteorológicos Simepar Paraná"
+page_icon = "☁️"
 
+st.set_page_config(page_title=page_title, page_icon=page_icon)
+st.title(page_title)
+# -------------------------------------------
 
-# Load the data from a CSV. We're caching this so it doesn't reload every time the app
-# reruns (e.g. if the user interacts with the widgets).
-#@st.cache_data
-def load_data():
-    df = pd.read_csv("data/dados_meteorologicos_simepar_parana.csv")
-
+@st.cache_data
+def load_json(file_path):
+    with open(file_path, 'r') as jsonFile:
+        f = json.load(jsonFile)
+        return f
+    
+@st.cache_data
+def load_geo_json_data(file_path):
+    with open(file_path) as f:
+        geojson_data = json.load(f)
+        return geojson_data
+    
+def load_csv_data(file_path):
+    df = pd.read_csv(file_path)
     return df
 
-#carregando coordenadas das estações
-with open('data/coordenadas_cidades.json', 'r') as arquivo:
-    estacoes = json.load(arquivo)   
+metrics = ["Temperatura Média",
+            "Umidade Relativa",
+            "Velocidade do Vento",
+            "Rajada do Vento",
+            "Precipitação Acumulada",
+            "Pressão Atmosférica Reduzida"]
 
-
-
-metricas = ["Temperatura Média","Umidade Relativa","Velocidade do Vento","Rajada do Vento","Precipitação Acumulada","Pressão Atmosférica Reduzida"]
-
-
-df = load_data()
-df.fillna(0, inplace=True)
-df["Data"] = pd.to_datetime(df["Data"])
-
-# Show a multiselect widget with the genres using `st.multiselect`.
-# cidade = st.multiselect(
-#     "Estações Metereológicas",
-#     df.Cidade.unique(),
-#     ["Apucarana","Palmas","Guaratuba"]
-# )
-
-cidade = st.selectbox(
-    "Estação Metereológica",
-    df["Cidade"].unique(),
-    3
-)
-
-
-
-# Select the metrics to analyse
-metrica = st.selectbox(
-    "Métrica",
-    metricas,
-    index=4
-)
-
-
-st.write(f'Registros na base de dados: {df["Data"].min().date()} até {df["Data"].max().date()}')
-#dates = st.date_input("Selecione um período",(datetime.date(2024,7,1),datetime.date(2024,7,3)),min_value=datetime.date(2024,6,30),max_value=datetime.date(2024,7,3))
-data = st.date_input("Selecione uma data",df["Data"].max()-datetime.timedelta(days=1),min_value=df["Data"].min(),max_value=df["Data"].max())
-
-
-data = pd.to_datetime(data)
-
-
-#Filter the dataframe based on the widget input and reshape it.
-df_filtered = df[(df["Cidade"] == cidade) & (df["Data"] == data)]
-
-
-df_reshaped = df_filtered.pivot_table(
-    index="Horario", columns="Cidade", values=metrica, fill_value=0
-)
-
-print(df_reshaped)
-
-#
-#Display the data as a table using `st.dataframe`.
-# st.dataframe(
-#     df_reshaped,
-#     use_container_width=True,
-#     column_config={"Horario": st.column_config.TextColumn("Horário")},
-# )
-
-# Display the data as an Altair chart using `st.altair_chart`.
-df_chart = pd.melt(
-    df_reshaped.reset_index(), id_vars="Horario", var_name="Cidade", value_name=metrica
-)
-
-unidadeMetrica = {
+metrics_unit = {
     "Temperatura Média": "(ºC)",
     "Umidade Relativa": "(%)",
     "Velocidade do Vento": "(Km/h)",
@@ -106,12 +50,58 @@ unidadeMetrica = {
     "Pressão Atmosférica Reduzida": "(hPa)"
 }
 
+
+stations_coordinates = load_json("data/coordenadas_estacoes.json")
+meteorological_data = load_csv_data("data/dados_meteorologicos_simepar_parana.csv")
+
+#processing data
+meteorological_data.fillna(0, inplace=True)
+meteorological_data["Data"] = pd.to_datetime(meteorological_data["Data"])
+
+st.write(
+    """
+    Este aplicativo visualiza dados do [SIMEPAR](http://www.simepar.br/prognozweb/simepar).
+    Ele mostra métricas sobre o clima coletadas em estações meteorológicas no Paraná-RS.
+    """
+)
+
+selected_station = st.selectbox(
+    "Estação Metereológica",
+    meteorological_data["Cidade"].unique(),
+    3
+)
+
+# Select the metrics to analyse
+selected_metric = st.selectbox(
+    "Métrica",
+    metrics,
+    index=4
+)
+
+st.write(f'Registros na base de dados: {meteorological_data["Data"].min().date()} até {meteorological_data["Data"].max().date()}')
+date = st.date_input("Selecione uma data",meteorological_data["Data"].max()-datetime.timedelta(days=1),min_value=meteorological_data["Data"].min(),max_value=meteorological_data["Data"].max())
+
+
+#Filter the dataframe based on the widget input and reshape it.
+df_filtered = meteorological_data[(meteorological_data["Cidade"] == selected_station) & (meteorological_data["Data"] == pd.to_datetime(date))]
+
+
+df_reshaped = df_filtered.pivot_table(
+    index="Horario", columns="Cidade", values=selected_metric, fill_value=0
+)
+
+# Display the data as an Altair chart using `st.altair_chart`.
+df_chart = pd.melt(
+    df_reshaped.reset_index(), id_vars="Horario", var_name="Cidade", value_name=selected_metric
+)
+
+
 chart = (
     alt.Chart(df_chart)
     .mark_line()
     .encode(
         x=alt.X("Horario:N", title="Horário"),
-        y=alt.Y(f'{metrica}:Q', title=f'{metrica} {unidadeMetrica[metrica]}'),
+        y=alt.Y(f'{selected_metric}:Q', title=f'{selected_metric} {metrics_unit[selected_metric]}'),
         color="Cidade:N"
     )
     .properties(height=320)
@@ -119,141 +109,130 @@ chart = (
 st.altair_chart(chart, use_container_width=True)
 
 
-optionChuva = st.selectbox(
+#Days without rain Section
+city_option = st.selectbox(
     "Selecione uma Estação",
-    df.Cidade.unique(),
+    meteorological_data.Cidade.unique(),
     index=3
 )
 
-#Calculando os dias sem chuva
-data = df["Data"].max()
-periodo = data - df["Data"].min()
-count = 0
+#calculating days without rain
+def days_without_rain(start_date,total_period_in_days):
 
-print(data)
-print()
+    count = 0
+    date = start_date
+    for i in range(0,total_period_in_days):
+        day = meteorological_data[(meteorological_data["Data"] == date) & (meteorological_data["Cidade"] == city_option)]
+        max_precipitation = day["Precipitação Acumulada"].sum()
+        if(max_precipitation <= 0):
+            count += 1
+        else:
+            return count
+        date -= datetime.timedelta(days=1)
 
+start_date = meteorological_data["Data"].max()
+total_period_in_days = (start_date - meteorological_data["Data"].min()).days
 
-for i in range(0,periodo.days):
-    dia = df[(df["Data"] == data) & (df["Cidade"] == optionChuva)]
-    precipitacaoMax = dia["Precipitação Acumulada"].max()
-    if(precipitacaoMax <= 0):
-        count += 1
-    else:
-        break
-    data += datetime.timedelta(days=-1)
+count = days_without_rain(start_date,total_period_in_days)
 
 st.write(f'# Dias sem chuva: {count}')
 
 
-classificacaoChuva = ""
+if(count < total_period_in_days):
 
-if precipitacaoMax < 2.5:
-    classificacaoChuva = "Fraca < 2.5mm"
-elif precipitacaoMax < 10:
-    classificacaoChuva = "Moderada < 10mm"
-elif precipitacaoMax > 50:
-    classificacaoChuva = "Forte > 50mm"
+    last_day_with_rain = meteorological_data["Data"].max() - datetime.timedelta(days=count)
+    precipitation_last_day_with_rain = (meteorological_data[(meteorological_data["Data"] == last_day_with_rain) & (meteorological_data["Cidade"] == city_option)])["Precipitação Acumulada"].sum()
 
-if(precipitacaoMax != 0 and precipitacaoMax != np.nan):
-    st.write(f'Último dia com chuva: {data.date()}: {precipitacaoMax}mm - Classificação: {classificacaoChuva}')
+    rain_classification = ""
+
+    if precipitation_last_day_with_rain <= 10:
+        rain_classification = "Fraca <= 10mm"
+    elif precipitation_last_day_with_rain > 10 and precipitation_last_day_with_rain <= 50:
+        rain_classification = "Moderada - entre 10mm e 50mm"
+    elif precipitation_last_day_with_rain > 50:
+        rain_classification = "Forte > 50mm"
+
+    st.write(f'Último dia com chuva: {last_day_with_rain.date()}: {precipitation_last_day_with_rain:.2f}mm - Classificação: {rain_classification}')
 else:
     st.write(f'Último dia com chuva: Sem dados!')
 
+
+#Show the City Map
 def get_lat_long(city):
-    location = estacoes[city]
+    location = stations_coordinates[city]
     if location:
         return location["latitude"], location["longitude"]
     else:
         return None, None
 
-latitude, longitude = get_lat_long(optionChuva)
 
+def create_city_map(city_option):
+    latitude, longitude = get_lat_long(city_option)
 
-m = folium.Map(location=[latitude, longitude], zoom_start=12, min_zoom=6, max_zoom=13)
+    city_map = folium.Map(location=[latitude, longitude], zoom_start=12, min_zoom=6, max_zoom=13)
 
-# Adicionando um marcador para a estação selecionada
-folium.Marker([latitude, longitude], tooltip=optionChuva).add_to(m)
+    folium.Marker([latitude, longitude], tooltip=city_option).add_to(city_map)
 
-# Exibindo o mapa no Streamlit
-folium_static(m)
+    return city_map
 
+pr_map = create_city_map(city_option)
+folium_static(pr_map)
 
-#Teste Mapa de calor
-
-import folium
-import branca.colormap
-import numpy as np
-from folium.plugins import HeatMap
-from folium import Choropleth
-
-data = "./data/geojs-41-mun.json"
-# df_merged2 = pd.read_csv("conversaoDados/heat_map.csv",sep=",")
-# df_merged = pd.read_csv("./data/Interpolated_Precipitation_Data.csv",sep=",")
-
-df_base = pd.read_csv("./data/intepolation_base_cidades.csv",sep=",")
-
-df_period_total = df.groupby(['Cidade']).agg({'Precipitação Acumulada': 'sum'}).reset_index()
-
-df_period_total = df_period_total[["Cidade","Precipitação Acumulada"]]
-
-df_merged = df_base.merge(df_period_total,on="Cidade",how="outer")
-
-print(len(df_merged))
 
 def interpolateData(data=None):
-    # Separar os dados com precipitação (não NaN) e sem precipitação (NaN)
     known_precipitation = data.dropna(subset=['Precipitação Acumulada'])
     unknown_precipitation = data[data['Precipitação Acumulada'].isna()]
 
-    # Construir uma árvore KD para os pontos conhecidos
     tree = cKDTree(known_precipitation[['latitude', 'longitude']])
 
-    # Definir uma função de inverso da distância
     def inverse_distance_weighting(distances, values, num_nearest=3):
         inverse_distances = 1 / distances
         weights = inverse_distances / inverse_distances.sum()
         return np.dot(weights, values)
 
-    # Encontrar os pontos mais próximos e calcular a interpolação
     def interpolate_missing_values(row):
         distances, indices = tree.query([row['latitude'], row['longitude']], k=3)
         nearest_values = known_precipitation.iloc[indices]['Precipitação Acumulada'].values
         return inverse_distance_weighting(distances, nearest_values)
 
-    # Aplicar a interpolação para cada linha com precipitação desconhecida
     interpolated_values = unknown_precipitation.apply(interpolate_missing_values, axis=1)
 
-    # Atualizar o DataFrame original com os valores interpolados
     data.loc[data['Precipitação Acumulada'].isna(), 'Precipitação Acumulada'] = interpolated_values
 
     return data
 
+# Estimate precipitation on all cities 
+cities_geojson_data = load_geo_json_data("./data/geojs-41-mun.json")
 
-df_merged = interpolateData(df_merged)
+cities_lat_long = pd.read_csv("./data/lat_long_cidades_pr.csv",sep=",")
+
+df_period_total = meteorological_data.groupby(['Cidade']).agg({'Precipitação Acumulada': 'sum'}).reset_index()
+
+accumulated_precipitation_by_station = df_period_total[["Cidade","Precipitação Acumulada"]]
+
+accumulated_precipitation_by_city = cities_lat_long.merge(accumulated_precipitation_by_station,on="Cidade",how="outer")
+
+accumulated_precipitation_estimation = interpolateData(accumulated_precipitation_by_city)
 
 
-with open(data) as f:
-    geojson_data = json.load(f)
+#Precipitation map in the period
+pr_map = folium.Map(location=[-24.5452, -51.5652], zoom_start=7)
 
-m = folium.Map(location=[-24.5452, -51.5652], zoom_start=7)
-
-estilo = lambda x: {"color" : "white",
+style = lambda x: {"color" : "white",
                    "fillOpacity": 1,
                    "weight": 1}
 
-# Adiciona os limites municipais ao mapa
 folium.GeoJson(
-    geojson_data,
+    cities_geojson_data,
     name='geojson',
-    style_function=estilo
-).add_to(m)
+    style_function=style
+).add_to(pr_map)
 
 
 Choropleth(
-    geo_data=geojson_data,
-    name='choropleth',
-    data=df_merged,
+    geo_data=cities_geojson_data,
+    name='precipitação acumulada no período',
+    data=accumulated_precipitation_estimation,
     columns=['Cidade', 'Precipitação Acumulada'],
     key_on='feature.properties.name',
     fill_color='YlOrRd',
@@ -262,21 +241,8 @@ Choropleth(
     legend_name='Precipitação Acumulada (mm)',
     nan_fill_opacity=0.0,
     nan_fill_color="white"
-).add_to(m)
+).add_to(pr_map)
 
 
-#Adicionar as cidades ao mapa com a precipitação acumulada
-# for _, row in df_merged2.iterrows():
-#     folium.CircleMarker(
-#         location=[row['latitude'], row['longitude']],
-#         radius=0,
-#         popup=folium.Popup(f"{row['Cidade']}: {row['Precipitação Acumulada']} mm", parse_html=True),
-#         tooltip=folium.Tooltip(f"{row['Cidade']}: {row['Precipitação Acumulada']} mm"),
-#         color='orange',
-#         fill=True,
-#         fill_color='orange',
-#     ).add_to(m)
-
-# Exibir o mapa no Streamlit
 st.title('Mapa de Precipitação Acumulada no Período')
-folium_static(m)
+folium_static(pr_map)
